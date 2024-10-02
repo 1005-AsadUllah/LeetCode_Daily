9aabc85f67d599d9a10d169d4166bec375615d86
1005-AsadUllah <155570310+1005-AsadUllah@users.noreply.github.com>
2024-10-02 21:08:36 +0600

    Time: 108 ms (20.38%), Space: 51.3 MB (9.29%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& a) {
+        int n = a.size();
+            if(n== 0)
+                    return {};
+            set<int> s(a.begin(),a.end());
+            
+            unordered_map<int,int> mp;
+            int r =1;
+            for(auto val : s)
+            {
+                    mp[val] = r;
+                    r++;
+            }
+            vector<int> ans;
+            
+            for(auto val : a)
+            {
+                  ans.push_back(mp[val]);  
+            }
+            return ans;
+    }
+};
